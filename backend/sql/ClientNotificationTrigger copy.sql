787bb262ebd7753aa901551b3bfba16641a2e0e8
JuanLuisLopez-code <juanluislopezdaw@gmail.com>
2023-02-15 21:45:59 +0100

    Trigger notifications & Notifications END

---
+++ backend/sql/ClientNotificationTrigger copy.sql
@@ -0,0 +1,13 @@
+delimiter //
+CREATE TRIGGER user_user_AU
+AFTER UPDATE ON user_user
+FOR EACH ROW
+BEGIN
+IF NEW.tokenForgotPassword IS NOT NULL THEN
+INSERT INTO incidents_notification (body, user_id, seen, created_at, modified_at) VALUES (CONCAT('Please put a new password.'), OLD.id, FALSE, NOW(), NOW());
+END IF;
+IF NEW.tokenForgotPassword IS NULL THEN INSERT INTO incidents_notification (body, user_id, seen, created_at, modified_at) VALUES 
+(CONCAT('Your new password is updated correctly.'), OLD.id, FALSE, NOW(), NOW());
+END IF;
+END//
+delimiter ;
